920b969494d51dc958e0a71e033a646a0c225bea
RosiAvramova <106426969+RosiAvramova@users.noreply.github.com>
2023-05-16 14:11:47 +0300

    Initial commit

---
+++ Exam Preparation l/AnimalCentre/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AnimalCentre.Core;
+using AnimalCentre.Core.Contracts;
+using AnimalCentre.Models.Animals;
+using AnimalCentre.Models.Contracts;
+
+namespace AnimalCentre
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            IEngine engine = new Engine();
+            engine.Run();
+
+        }
+    }
+}
---
+++ Exam Preparation ll/SoftUniRestaurant/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SoftUniRestaurant.Core;
+
+namespace SoftUniRestaurant
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            RestaurantController restaurantController = new RestaurantController();
+            Engine engine = new Engine(restaurantController);
+
+            engine.Run();
+        }
+    }
+}
